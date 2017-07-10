38df4c3b1caac1902bdd8fab2304de1fd8286cd6
kwrush <kaiwang.me@gmail.com>
2017-07-10 22:07:45 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import normalized from './css/normalize.css';
+import style from './css/main.css';
+import Game from './components/Game';
+
+ReactDOM.render(<Game />, document.getElementById('game'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: '.src/index.js',
+    output: {
+        path: path.resolve(__dirnmae, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract('css-loader')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ],
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        port: 8080
+    }
+};
