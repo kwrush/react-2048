cf0329c972687faafbe26a67a987ac34671b6346
kwrush <kaiwang.me@gmail.com>
2021-03-28 01:06:30 +0100

    Update style of scale controller

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -26,57 +26,57 @@ const Control: FC<ControlProps> = ({
       </Text>
     </Button>
     <Box>
-      <Box marginInlineEnd="s3">
-        <Box marginInlineEnd="s1">
-          <Text textTransform="uppercase" fontSize={13} color="primary">
-            rows:
-          </Text>
+      <Box marginInlineEnd="s5" flexDirection="column">
+        <Text textTransform="uppercase" fontSize={13} color="primary">
+          rows
+        </Text>
+        <Box padding="s2">
+          <Button
+            mini
+            onClick={() => onChangeRow(-1)}
+            disable={rows === MIN_SCALE}
+          >
+            -
+          </Button>
+          <Box marginInline="s2">
+            <Text fontSize={16} color="primary">
+              {rows}
+            </Text>
+          </Box>
+          <Button
+            mini
+            onClick={() => onChangeRow(1)}
+            disable={rows === MAX_SCALE}
+          >
+            +
+          </Button>
         </Box>
-        <Button
-          mini
-          onClick={() => onChangeRow(-1)}
-          disable={rows === MIN_SCALE}
-        >
-          -
-        </Button>
-        <Box marginInline="s2">
-          <Text fontSize={16} color="primary">
-            {rows}
-          </Text>
-        </Box>
-        <Button
-          mini
-          onClick={() => onChangeRow(1)}
-          disable={rows === MAX_SCALE}
-        >
-          +
-        </Button>
       </Box>
-      <Box>
-        <Box marginInlineEnd="s1">
-          <Text textTransform="uppercase" fontSize={13} color="primary">
-            cols:
-          </Text>
+      <Box flexDirection="column">
+        <Text textTransform="uppercase" fontSize={13} color="primary">
+          cols
+        </Text>
+        <Box padding="s2">
+          <Button
+            mini
+            onClick={() => onChangeCol(-1)}
+            disable={cols === MIN_SCALE}
+          >
+            -
+          </Button>
+          <Box marginInline="s2">
+            <Text fontSize={16} color="primary">
+              {cols}
+            </Text>
+          </Box>
+          <Button
+            mini
+            onClick={() => onChangeCol(1)}
+            disable={cols === MAX_SCALE}
+          >
+            +
+          </Button>
         </Box>
-        <Button
-          mini
-          onClick={() => onChangeCol(-1)}
-          disable={cols === MIN_SCALE}
-        >
-          -
-        </Button>
-        <Box marginInline="s2">
-          <Text fontSize={16} color="primary">
-            {cols}
-          </Text>
-        </Box>
-        <Button
-          mini
-          onClick={() => onChangeCol(1)}
-          disable={cols === MAX_SCALE}
-        >
-          +
-        </Button>
       </Box>
     </Box>
   </Box>
