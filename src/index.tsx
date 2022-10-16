789e1d714c96803577a699b76fae2538287b9665
kwrush <kaiwang.me@gmail.com>
2022-10-16 20:29:36 +0200

    Migrate to vite

--- src/index.tsx
+++ src/index.tsx
@@ -10,19 +10,3 @@ ReactDOM.render(
   </>,
   document.getElementById('game'),
 );
-
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    const pathname = window.location.pathname.replace(/\/$/g, '');
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register(`${pathname}/service-worker.js`)
-        .then((registration) => {
-          // eslint-disable-next-line no-console
-          console.log('SW registered:', registration);
-        })
-        // eslint-disable-next-line no-console
-        .catch((e) => console.error('SW registration failed', e));
-    });
-  }
-}
